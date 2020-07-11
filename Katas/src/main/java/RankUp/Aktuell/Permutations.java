f88123a32f050f57000c6428e0ea3974b8c1a291
jaess105 <jaess105@uni-duesseldorf.de>
2020-07-11 09:43:54 +0200

    Imperativer Style

--- Katas/src/main/java/RankUp/Aktuell/Permutations.java
+++ Katas/src/main/java/RankUp/Aktuell/Permutations.java
@@ -1,4 +1,28 @@
 package RankUp.Aktuell;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Permutations {
+    public static List<String> singlePermutations(String s) {
+        // Your code here!
+        //int start=0;
+        ArrayList<String> liste=new ArrayList<>();
+        for(Character character:s.toCharArray()){
+            liste.add(character.toString());
+        }
+        ArrayList<String> ausgabe=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            String neuerString=Character.toString(s.charAt(i));
+            for(int k=0;k<s.length();k++){
+                if(i==k) k++;
+                if(k<s.length()) neuerString+=s.charAt(k);
+            }
+            ausgabe.add(neuerString);
+        }
+
+        return ausgabe;
+    }
 }
--- Katas/src/test/java/RankUp/Aktuell/Test_Permutations.java
+++ Katas/src/test/java/RankUp/Aktuell/Test_Permutations.java
@@ -1,4 +1,38 @@
 package RankUp.Aktuell;
 
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Test_Permutations {
+
+
+       @Test
+       public void example1() {
+           assertThat(  Permutations.singlePermutations("a").stream()
+                   .sorted()
+                   .collect(Collectors.toList()))
+                   .isEqualTo(new ArrayList<String>(Arrays.asList("a")));
+            }
+
+            @Test
+            public void example2() {
+                assertThat( Permutations.singlePermutations("ab").stream()
+                        .sorted()
+                        .collect(Collectors.toList()) )
+                        .isEqualTo(new ArrayList<String>(Arrays.asList("ab","ba")));
+            }
+
+            @Test
+            public void example3() {
+                assertThat(Permutations.singlePermutations("aabb").stream()
+                        .sorted()
+                        .collect(Collectors.toList()) )
+                        .isEqualTo(new ArrayList<String>(Arrays.asList("aabb", "abab", "abba", "baab", "baba", "bbaa")));
+            }
 }
