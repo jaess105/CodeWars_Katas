01f65b36cfe1a061be8c53031a306f4e85bcff35
jaess105 <jaess105@uni-duesseldorf.de>
2020-09-26 20:28:32 +0200

    update last text

--- Katas/src/main/java/Fundamentals/CountingDuplicates.java
+++ Katas/src/main/java/Fundamentals/CountingDuplicates.java
@@ -12,11 +12,9 @@ Count the number of Duplicates
         "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
         "aA11" -> 2 # 'a' and '1'
         "ABBA" -> 2 # 'A' and 'B' each occur twice
+        https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/solutions/java
+        https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/java
 */
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
