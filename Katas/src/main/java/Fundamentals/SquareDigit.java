4daa5b7e3bf2f81a0db08193d173fc7e32b97a48
jaess105 <jaess105@uni-duesseldorf.de>
2020-09-26 18:09:30 +0200

    Added a Fundamental Kata Solution

---
+++ Katas/src/main/java/Fundamentals/SquareDigit.java
@@ -0,0 +1,20 @@
+package Fundamentals;
+/*
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+Note: The function accepts an integer and returns an integer
+ */
+
+
+import static java.util.stream.Collectors.joining;
+
+public class SquareDigit {
+
+    public int squareDigits(int n) {
+        return Integer.parseInt(  //The endproduct is a String because we need to join the integers and not add them
+                String.valueOf(n)  //converting to String to stream over every singel digit
+                        .chars()  //a Integer Stream of the Char value
+                        .mapToObj(x->String.valueOf((int)Math.pow((x-48),2))) //48 must be subtracted from x because x is the char representation. Then it is risen by the power oft 2
+                        .collect(joining())); //collecting every risen number by conncating them in one String
+    }
+}
---
+++ Katas/src/test/java/Fundamentals/SquareDigitTest.java
@@ -0,0 +1,12 @@
+package Fundamentals;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareDigitTest {
+    @Test
+    public void test() {
+        assertEquals(811181, new SquareDigit().squareDigits(9119));
+    }
+
+}
