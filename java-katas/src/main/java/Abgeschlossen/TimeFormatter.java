02098014e9ed09c239fda85eaf714d8ad8c00615
Jannik Esser <jaess105@uni-duesseldorf.de>
2021-04-14 09:33:23 +0200

    TimeFormat Kata revisited

--- java-katas/src/main/java/Abgeschlossen/TimeFormatter.java
+++ java-katas/src/main/java/Abgeschlossen/TimeFormatter.java
@@ -2,7 +2,31 @@ package Abgeschlossen;
 
 public class TimeFormatter {
 
-    public static String formatDuration(int seconds) {
+  public static String formatDuration(int seconds) {
+    if(seconds == 0)
+      return "now";
+    int yearInSec = 31536000, dayInSec = 86400, hourInSec = 3600, minuteInSec = 60;
+    String converted = new StringBuilder()
+        .append(format(seconds / yearInSec, "year"))
+        .append(format(seconds % yearInSec / dayInSec, "day"))
+        .append(format(seconds % dayInSec / hourInSec, "hour"))
+        .append(format(seconds % hourInSec / minuteInSec, "minute"))
+        .append(format(seconds % 60, "second"))
+        .toString()
+        .replaceAll("(, $)", "");
+    return replaceLast(converted, ","," and");
+  }
+
+  static String format(int time, String timeDeclarer) {
+    return time < 1 ? "" : time + " " + (time == 1 ? timeDeclarer + ", " : timeDeclarer + "s, ");
+  }
+
+  static String replaceLast(String text, String regex, String replacement) {
+    return text.replaceFirst("(?s)" + regex + "(?!.*?" + regex + ")", replacement);
+  }
+
+
+    /*public static String formatDuration(int seconds) {
         // your code goes here
         int year=31536000,day=86400, hour=3600, minute =60;
         int [] time={0,0,0,0,0};
@@ -49,7 +73,7 @@ public class TimeFormatter {
             }
         }
         return ausgabe;
-    }
+    }*/
 }
 
  /*
