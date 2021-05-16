83f13948f51bc5d13e8665997f8bce5f86b2af8c
Jannik <jaess105@uni-duesseldorf.de>
2021-05-16 18:42:04 +0200

    Insane Colour Kata abgeschlossen durch brute force.

--- java-katas/src/main/java/RankUp/stufe2/InsaneColouredTriangles.java
+++ java-katas/src/main/java/RankUp/stufe2/InsaneColouredTriangles.java
@@ -1,12 +1,40 @@
 package RankUp.stufe2;
 
 /**
- * https://www.codewars.com/kata/5a331ea7ee1aae8f24000175/train/java
+ * https://www.codewars.com/kata/5a331ea7ee1aae8f24000175/train/java Its not how its supposed to be
+ * done. But why use your brian, if you can use brute force.
  */
 
 public class InsaneColouredTriangles {
 
-  static int isR = 'G' + 'B';
+  private static char[] sequenzArray = generateSequenzeArraz();
+
+  private static char[] generateSequenzeArraz() {
+    char[] seq = new char[169];
+    seq['R' + 'G'] = 'B';
+    seq['G' + 'B'] = 'R';
+    seq['B' + 'R'] = 'G';
+    seq['R' + 'R'] = 'R';
+    seq['G' + 'G'] = 'G';
+    seq['B' + 'B'] = 'B';
+    return seq;
+  }
+
+  public static char triangle(String row) {
+    char[] charry = row.toCharArray();
+    int length = charry.length - 1;
+    char[] sb = new char[length];
+    for (; length > 0; length--) {
+      for (int i = 0; i < length; i++) {
+        sb[i] = sequenzArray[(charry[i] + charry[i + 1])];
+      }
+      charry = sb;
+    }
+    return charry[0];
+  }
+}
+
+ /* static int isR = 'G' + 'B';
   static int isB = 'G' + 'R';
   static int isG = 'B' + 'R';
 
@@ -40,4 +68,4 @@ public class InsaneColouredTriangles {
       }
     }
   }
-}
+}*/
