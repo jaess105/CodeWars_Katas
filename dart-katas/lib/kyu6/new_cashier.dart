57489b6d17e4f68961c11a809ea6d5fdd3078d4a
Jannik <jaess105@uni-duesseldorf.de>
2022-04-11 13:59:09 +0200

    new cashier kata

---
+++ dart-katas/lib/kyu6/new_cashier.dart
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5d23d89906f92a00267bb83d/train/dart
+
+const _menu = [
+  "Burger",
+  "Fries",
+  "Chicken",
+  "Pizza",
+  "Sandwich",
+  "Onionrings",
+  "Milkshake",
+  "Coke"
+];
+
+String getOrder(String input) => _menu
+    .fold<Map<String, int>>(<String, int>{}, (map, order) {
+      map[order] = order.toLowerCase().allMatches(input).length;
+      return map;
+    })
+    .entries
+    .fold<String>("", (str, entry) => str + ((entry.key + ' ') * entry.value))
+    .trim();
