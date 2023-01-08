d784cc7f6bc412953292ba0d76d4ef5b37166a16
Jannik <jaess105@uni-duesseldorf.de>
2023-01-08 17:52:09 +0100

    added new Kata

--- csharp/CSharp_Katas/Katas/Kyu5/ProdFib.cs
+++ csharp/CSharp_Katas/Katas/Kyu5/ProdFib.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace Katas.Kyu5
 {
+    // https://www.codewars.com/kata/5541f58a944b85ce6d00006a/train/csharp
     public static class ProdFib
     {
         public static ulong[] productFib(ulong prod)
         {
-            return null;
+            var fib = new List<ulong>() { 0, 1, 1 };
+            int i = 3;
+            for (; fib[i - 1] * fib[i - 2] < prod; i++)
+            {
+                fib.Add(fib[i - 1] + fib[i - 2]);
+            }
+
+            return new ulong[] {
+                fib[i - 2], fib[i - 1],  (prod % (ulong) (fib[i - 1] * fib[i - 2]) == 0 ? 1 : 0UL)
+            };
         }
     }
 }
---
+++ csharp/CSharp_Katas/TestProject/Kyu5/ProbFibTest.cs
@@ -0,0 +1,18 @@
+﻿using Katas.Kyu5;
+using NUnit.Framework;
+
+namespace TestProject.Kyu5
+{
+    [TestFixture]
+    public class ProdFibTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            ulong[] r = new ulong[] { 55, 89, 1 };
+            Assert.AreEqual(r, ProdFib.productFib(4895));
+        }
+    }
+
+}
