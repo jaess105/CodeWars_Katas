be4a4be124c7b15821c38b907d453fe32a6b0f61
jaess105 <jaess105@uni-duesseldorf.de>
2025-03-30 14:40:26 +0200

    Fixed: there is no - operation...

--- fsharp/FSharpKatas/FSharpKatas/Kyu6/MiniStringFuck.fs
+++ fsharp/FSharpKatas/FSharpKatas/Kyu6/MiniStringFuck.fs
@@ -1,18 +1,12 @@
+/// https://www.codewars.com/kata/586dd26a69b6fd46dd0000c0/train/fsharp
 module FSharpKatas.Kyu6.MiniStringFuck
 
-let inc (ctr: int) = if ctr >= 255 then 0 else ctr + 1
-let dec (ctr: int) = if ctr <= 0 then 255 else ctr - 1
-
 let interpret (code: string) =
-    let rec interpretRec (code: string) (ctr: int) (res: char list) =
+    let rec interpretRec (code: char list) (ctr: int) (res: char list) =
         match Seq.toList code with
         | [] -> res
-        | op :: rest when op = '+' -> interpretRec (rest |> System.String.Concat) (inc ctr) res
-        | op :: rest when op = '_' -> interpretRec (rest |> System.String.Concat) (dec ctr) res
-        | op :: rest when op = '.' ->
-            let c: char = (char ctr)
-            interpretRec (rest |> System.String.Concat) ctr ([ c ] |> List.append res)
-        | _ :: rest -> interpretRec (rest |> System.String.Concat) ctr res
+        | op :: rest when op = '+' -> interpretRec rest ((ctr + 1) % 256) res
+        | op :: rest when op = '.' -> interpretRec rest ctr ([ (char ctr) ] |> List.append res)
+        | _ :: rest -> interpretRec rest ctr res
 
-    let result = (interpretRec code 0 [])
-    result |> System.String.Concat
+    (interpretRec (Seq.toList code) 0 []) |> System.String.Concat
